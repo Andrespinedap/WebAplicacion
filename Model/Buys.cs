be0b34a9b991f2e9deca5fb36662590e2d1d845d
ANDRES PINEDA <ANDRES PINEDA@LAPTOP-J09D2M4M>
2024-09-27 18:16:49 -0500

    se suben las clases

---
+++ Model/Buys.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAplicacion.Model
+{
+    //tabla compra
+    public class Buys
+    {
+        public int Id { get; set; }
+        public string supplier_Id { get; set; }
+        public string Date { get; set; }
+        public int Total { get; set; }
+    }
+}
